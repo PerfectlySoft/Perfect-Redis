a9152ffc4d0025048dd03f93dc9ccbb6b6f218cb
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-06-11 16:20:32 -0400

    RedisHash

---
+++ Sources/RedisHash.swift
@@ -0,0 +1,35 @@
+//
+//  RedisHash.swift
+//  PerfectRedis
+//
+//  Created by Kyle Jessup on 2018-06-11.
+//
+
+import Foundation
+
+public struct RedisHash {
+	public typealias Element = (key: String, value: RedisClient.RedisValue)
+	public typealias KeyType = String
+	public typealias ValueType = RedisClient.RedisValue
+	let client: RedisClient
+	let name: String
+	public init(_ client: RedisClient, name: String) {
+		self.client = client
+		self.name = name
+	}
+	public subscript(_ key: KeyType) -> ValueType? {
+		get {
+			guard let r = try? client.hashGet(key: name, field: key) else {
+				return nil
+			}
+			return r.value
+		}
+		set {
+			guard let newValue = newValue else {
+				_ = try? client.hashDel(key: name, fields: key)
+				return
+			}
+			_ = try? client.hashSet(key: name, field: key, value: newValue)
+		}
+	}
+}
